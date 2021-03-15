6c76ff2a579df820bbd557a945a5d47496dc1439
Patchrik <pklly92@gmail.com>
2021-03-15 10:12:35 -0500

    Set up and imported our API mock up

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,18 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import { FAKE_MOVIE_DETAILS, FAKE_SEARCH_RESULTS } from "./Fake_DATA";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    searchTerm: "home alone",
+    movieResults: FAKE_SEARCH_RESULTS,
+    movieDetails: FAKE_MOVIE_DETAILS,
+    movieReviews: [],
+  },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
 });
