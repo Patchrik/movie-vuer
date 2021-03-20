adbba9410cadc0aab88ae69b72882b27abca1251
Patchrik <pklly92@gmail.com>
2021-03-20 10:47:21 -0500

    just set up the app to work on a mobile device better.

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,27 +1,33 @@
 <template>
-  <v-app-bar app color="primary" dark>
-    <v-row justify="space-between" align="center">
-      <v-col :xs="6" :sm="6" :md="6" :lg="6" :xl="6">
-        <div class="d-flex align-center title-header">
-          <h1 class="ma-1 pa-1">where to vue</h1>
-        </div>
-      </v-col>
-
-      <v-col :sm="2" class="mt-5">
-        <form @submit.prevent="handleSubmit">
-          <v-text-field
-            v-model="query"
-            dense
-            rounded
-            filled
-            placeholder="Search movie titles"
-            background-color="indigo darken-2"
-            prepend-inner-icon="mdi-magnify"
-            class="search-box"
-          ></v-text-field>
-        </form>
-      </v-col>
-    </v-row>
+  <v-app-bar v-if="!isMobile" app color="primary" dark>
+    <v-app-bar-title>Where To Vue</v-app-bar-title>
+    <v-spacer></v-spacer>
+    <form @submit.prevent="handleSubmit">
+      <v-text-field
+        v-model="query"
+        dense
+        rounded
+        filled
+        placeholder="Search movie titles"
+        background-color="indigo darken-2"
+        prepend-inner-icon="mdi-magnify"
+        class="mt-6"
+      ></v-text-field>
+    </form>
+  </v-app-bar>
+  <v-app-bar v-else app color="primary" dark>
+    <form @submit.prevent="handleSubmit">
+      <v-text-field
+        v-model="query"
+        dense
+        rounded
+        filled
+        placeholder="Where To Vue"
+        background-color="indigo darken-2"
+        prepend-inner-icon="mdi-magnify"
+        class="mt-6 ml-15"
+      ></v-text-field>
+    </form>
   </v-app-bar>
 </template>
 
@@ -39,6 +45,16 @@
 
       handleSubmit() {
         this.searchMovies(this.query);
+      },
+    },
+    computed: {
+      isMobile() {
+        let screenWidth = screen.width;
+        if (screenWidth < 600) {
+          return true;
+        } else {
+          return false;
+        }
       },
     },
   };
